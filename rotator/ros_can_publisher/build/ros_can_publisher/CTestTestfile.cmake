46e37ac98a576d91859a9ccd4f3efd0d71d22023
Timoteus Ruotsalainen <timoteus.ruotsalainen@iki.fi>
2019-04-16 20:01:31 +0300

    initial workspace for publishing can data over ros

---
+++ rotator/ros_can_publisher/build/ros_can_publisher/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dev/git/satNOG/rotator/ros_can_publisher/src/ros_can_publisher
+# Build directory: /home/dev/git/satNOG/rotator/ros_can_publisher/build/ros_can_publisher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
